e942724d41cbcd740a6f0221ff67adf1ea1fd1cf
wrvr <974054997@qq.com>
2023-10-11 01:19:10 +0800

    add gitignore

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const commd = require("./commd")
 const fs = require('fs')
 const path = require('path')
 const outputs = require("./outputs")
-const text1 = `exec -p wrvrr
+const text1 = `exec -p wrvr -w test.txt gb2312
 echo hello
 `
 commd(text1, outputs()).then(result => console.log(result)).catch(err => console.error(err))
