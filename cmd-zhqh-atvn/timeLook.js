e4c9198c09ba4822d9f90a0b81bf4ca574d8c59f
wrvr <974054997@qq.com>
2023-12-31 14:57:38 +0800

    upgrade gettime tsjq

---
+++ cmd-zhqh-atvn/timeLook.js
@@ -0,0 +1,17 @@
+String.prototype.trimLines = function () {
+    return this.trim().split(/\n/).map(ele => ele.trim()).join('\n')
+}
+const timeLook = async (user_params = { lastParams: "", file: "", encoding: "utf8", head: false, tail: false, reverse: false }, outputs = { outputText }, neig_kp = {}) => {
+    outputs.outputText = (() => {
+        const bnll_zdti = new Date()
+        if (user_params._[1] === "human") {
+            return bnll_zdti.toString().replace(/ GMT.*/, '')
+        } else if (/^\d+$/.test(user_params._[1])) {
+            return new Date(user_params._[1]).toString().replace(/ GMT.*/, '')
+        } else {
+            return bnll_zdti.getTime()
+        }
+    })()
+
+}
+module.exports = timeLook
