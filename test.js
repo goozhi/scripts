4d6c5c2e2c2a4ba44dcd4fe99a3a71300c00bd78
wrvr <974054997@qq.com>
2024-01-11 22:19:28 +0800

    rm test

--- test.js
+++
@@ -1,8 +0,0 @@
-const checkUrl = require("./checkUrl");
-
-checkUrl('http://192.168.43.32:9000', 'http://192.168.43.237:9000'
-    // , 'http://192.168.43.238:9000'
-    // , 'http://192.168.43.239:9000'
-    // , 'http://192.168.43.240:9000'
-    // , 'http://192.168.43.241:9000'
-    , 'http://192.168.43.242:9000').then(res => console.log(res)).catch(error => console.error(error))
