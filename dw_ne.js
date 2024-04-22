4650b39b208d6864e771b16bc96972512d0add52
wrvr <974054997@qq.com>
2024-04-22 22:34:37 +0800

    sdvu

--- dw_ne.js
+++ dw_ne.js
@@ -2,7 +2,9 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = {
+    name: "scripts",
     diwr_jplp_nikc: {
         [__dirname]: {}
-    }
+    },
+    aq_yndf_lplp: null // yf mb
 }
--- ne_ww.js
+++ ne_ww.js
@@ -6,18 +6,21 @@ const fs = require('fs')
  * @param {object} neig_wyzv bi neig ji dw neig nini tt dk jc znzk dk neig, db lq tszn bnll nini ah dw jkub dk nikc mr stgn zzuy.
  * @param {string} ce_nikc dw jkub nikc dk ce dk trig.
  */
-async function ne_ww(neig_wyzv = {}, ce_nikc = "") {
+async function ne_ww(neig_wyzv = {}, ce_nikc = "", neig_kp = { ce_wu: '' }) {
     if (!ce_nikc) {
         throw new Error(`csrf-err: aoao tszn ce nikc - ${ce_nikc}`)
+    }
+    if (neig_wyzv.aq_yndf_jplp) {
+        console.log(neig_wyzv.name + ' aq_yndf_jplp: ', neig_wyzv.aq_yndf_jplp)
     }
     return await Promise.all(Object.entries(neig_wyzv.diwr_jplp_nikc).map(async ([fo1, yg1]) => {
         if (fs.statSync(fo1).isFile()) {
             throw new Error(`ac huop ey rjqt dk neig - ${fo1}`)
         } else {
             if (fs.existsSync(fo1)) {
-                await nikc_jkub_v16(fo1, path.join(ce_nikc, path.basename(fo1)), { ignoreFiles: [/^\.git.*/, /^\..*/, /^(out|test)\/.*$/i] })
+                await nikc_jkub_v16(fo1, path.join(ce_nikc, neig_kp.ce_wu || path.basename(fo1)), { ignoreFiles: [/^\.git.*/, /^\..*/, /^(out|test)\/.*$/i] })
                     .then(res => {
-                        const rj1 = 'Done making "' + fo1 + '" dk files.'
+                        const rj1 = 'Done. Wyzv "' + fo1 + '" dk files.'
                         console.log(rj1)
                         return rj1
                     })
