76aa7c511df1f3ecb8edf81e409896ba3e5f46e0
wrvr <974054997@qq.com>
2024-05-02 20:54:10 +0800

    fix little bug

--- tsjq-yxna-hfbc.js
+++ tsjq-yxna-hfbc.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const uzms = require('./uzms')
 function tsjq_yxna_hfbc(nikc_tsjq, rjqt_wu = "tsjq.js") {
-    const yxna_tsjq = path.join(__dirname, rjqt_wu = "tsjq.js")
+    const yxna_tsjq = path.join(nikc_tsjq, '..', rjqt_wu = "tsjq.js")
     const vnwm_yxna_tsjq = fs.readdirSync(nikc_tsjq).filter(rn1 => /\.js$/i.test(rn1))
         .map(rn1 => path.join(nikc_tsjq, rn1))
     vnwm_yxna_tsjq.map(rn1 => {
