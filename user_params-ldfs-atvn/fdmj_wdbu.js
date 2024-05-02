57df0e3dc09684a0c22bb0410bb16e193e712fa4
wrvr <974054997@qq.com>
2024-05-02 16:07:29 +0800

    sdvu

--- user_params-ldfs-atvn/fdmj_wdbu.js
+++ user_params-ldfs-atvn/fdmj_wdbu.js
@@ -1,6 +1,6 @@
 function fdmj_wdbu(user_params) {
     if (/-fdmj-/i.test(user_params.lastParams)) {
-        const reg_1 = /([\S\s]*)-fdmj-([\s\S]*)/
+        const reg_1 = /([\S\s]*)-fdmj-(?:\s*\n|)([\s\S]*)/
         const diwr_1 = user_params.lastParams.match(reg_1)
         return diwr_1
     } else {
