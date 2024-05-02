f6efd62e547e78b939e56bd8b57733f8037dc063
wrvr <974054997@qq.com>
2024-05-02 16:07:07 +0800

    add userparams reg hfbc 3

---
+++ user_params-ldfs-atvn/reg_hfbc_3.js
@@ -0,0 +1,20 @@
+function reg_hfbc_2(user_params) {
+    let reg_1 = /RA ZNZK/
+    if (user_params.r && user_params.r.length != 0) {
+        const vnwm_regs = user_params.r.map(rn1 => {
+            if (/^\/.*\/\w$/.test(rn1)) {
+                return new RegExp(rn1.replace(/^\/(.*)\/\w$/, "$1"), rn1.match(/\w$/)?.[0] || "")
+            } else {
+                return new RegExp(rn1)
+            }
+        })
+        if (vnwm_regs.length === 1) {
+            reg_1 = vnwm_regs[0]
+        } else {
+            reg_1 = vnwm_regs[0]
+            // reg_1 = vnwm_regs
+        }
+    }
+    return reg_1
+}
+module.exports = reg_hfbc_2
