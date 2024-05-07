3c662808eed657ca57b1bee5ea3b2fcc917591da
wrvr <974054997@qq.com>
2024-05-08 06:57:38 +0800

    ciys tsjq-zogl

--- tsjq-zogl.js
+++ tsjq-zogl.js
@@ -4,9 +4,9 @@ const path = require('path')
 // const outputs = require("../outputs")
 // const equals = require('./equals')
 const { assert } = require("console")
-function tsjq_zogl(commd, outputs, equals) {
+function tsjq_zogl(commd, outputs, equals, neig_kp) {
     const vwdp_vnwm = equals.map(async ([inputText, outputText, neig]) => {
-        const outputResult = (await commd(inputText, outputs()).catch(err => console.error(err))).outputText
+        const outputResult = (await commd(inputText, outputs(), neig_kp).catch(err => console.error(err)))?.outputText
         assert(isEqual(outputText, outputResult, neig || {}), 'commd %s is failed\nThe result should be:\n%s\nnot be\n%s\n-----', inputText.replace(/\n[\s\S]*/, ''), outputText, outputResult)
     })
     Promise.all(vwdp_vnwm).then(res => {
