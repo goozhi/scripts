d8dfa028eb2d1b3b1f08717d961649aac2f1c1bf
wrvr <974054997@qq.com>
2024-05-07 10:52:38 +0800

    ym wu

--- zogl_atvn/tsjq_vcmi_zjzj.js
+++ zogl_atvn/tsjq_vcmi_zjzj.js
@@ -2,7 +2,7 @@ const path = require('path')
 const tsjq_vcmi_zogl = require('../tsjq_vcmi_zogl')
 const nikc_opr_zogl = path.resolve('opr_zogl')
 
-async function rjqt_tsjq_zjzj() {
+async function tsjq_vcmi_zjzj() {
     await tsjq_vcmi_zogl(nikc_opr_zogl).catch(err => console.error(err))
 }
-module.exports = rjqt_tsjq_zjzj
+module.exports = tsjq_vcmi_zjzj
