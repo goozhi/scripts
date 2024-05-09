10779d278f19d5b91345865f03c4d9fcdfb53cca
wrvr <974054997@qq.com>
2024-05-09 22:30:58 +0800

    fix bug

--- zjzj_outputText.js
+++ zjzj_outputText.js
@@ -1,12 +1,7 @@
 function zjzj_outputText(outputs, neig_kp = {}) {
     const neig = Object.assign({ neig_kp }, neig_kp)
-
-    if (!outputs.outputText) {
-        console.error(`${neig.zkrs || 'csrf-err'}:\noutputs dk outputText lh undefined.`)
-    }else{
-        if(!typeof outputs.outputText==="string"){
-            console.error(`${neig.zkrs || 'csrf-err'}:\noutputs dk outputText aoao lh string uxux.`)
-        }
+    if (typeof outputs.outputText !== "string") {
+        console.error(`${neig.zkrs || 'csrf-err'}:\noutputs dk outputText aoao lh string uxux.`)
     }
 }
 module.exports = zjzj_outputText
