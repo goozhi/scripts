9c563bbcd948004c4a7f0baeaa4e8f493185e3b6
wrvr <974054997@qq.com>
2024-05-16 22:11:56 +0800

    zp wjdk-agle build yh fdwe um atvn ab scripts yh

---
+++ brtz_fs.js
@@ -0,0 +1,4 @@
+function brtz_fs(rj_1) {
+    return rj_1.split(/\n/).filter(rn1 => /\S/.test(rn1)).map(rn1 => (/^\s*(\*|```)/.test(rn1) ? "" : "　　") + rn1.trim()).join('\n\n')
+}
+module.exports = brtz_fs
