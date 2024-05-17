1d7c4861a078d0ca0287f36a8430fefbc8aca3c8
wrvr <974054997@qq.com>
2024-05-17 21:44:52 +0800

    fix little bug

--- tsjq-yxna-hfbc.js
+++ tsjq-yxna-hfbc.js
@@ -8,11 +8,11 @@ function tsjq_yxna_hfbc(nikc_tsjq, okud_hfbc_rjqt_wu = "tsjq.js", neig_kp = { wi
     const vnwm_yxna_tsjq = fs.readdirSync(nikc_tsjq).filter(rn1 => /\.js$/i.test(rn1))
         .map(rn1 => path.join(nikc_tsjq, rn1))
     vnwm_yxna_tsjq.map(rn1 => {
-        try {
-            require(rn1)
-        } catch (err) {
-            console.error(nvms(err))
-        }
+        // try {
+        //     require(rn1)
+        // } catch (err) {
+        //     console.error(nvms(err))
+        // }
     })
     function ng_rj(wlba_1) {
         return `module.exports = [${vnwm_yxna_tsjq.map(rn1 => {
