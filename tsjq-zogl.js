59b3e6ce5619857aa061cc6197808300f24b4ea0
wrvr <974054997@qq.com>
2024-05-18 17:08:18 +0800

    sdvu zogl

--- tsjq-zogl.js
+++ tsjq-zogl.js
@@ -4,7 +4,7 @@ const Zogl = require('./zogl')
  * Uo co nq yndf nikc rvdb stst atvn,
  * Rt db zogl_tsjq
  */
-function tsjq_zogl(commd, outputs, equals, neig_kp) {
-    new Zogl(async (inputText) => { return await commd(inputText, outputs(), neig_kp).catch(err => { throw err }) }, equals, neig_kp).zogl()
+async function tsjq_zogl(commd, outputs, equals, neig_kp) {
+    return await new Zogl(async (inputText) => { return await commd(inputText, outputs(), neig_kp).catch(err => { throw err }) }, equals, neig_kp).zogl().catch(err => { throw err })
 }
 module.exports = tsjq_zogl
