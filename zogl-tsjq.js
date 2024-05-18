9e9dadce958275c61c95043cfbae88793964f269
wrvr <974054997@qq.com>
2024-05-18 15:16:38 +0800

    ciys zogl

--- zogl-tsjq.js
+++ zogl-tsjq.js
@@ -2,7 +2,8 @@ const Zogl = require("./zogl")
 /**
  * zhxm lw dizi zogl atvn.
  */
-function zogl_tsjq(tsjq_zhqh, outputs, arrC, equals, neig_kp = {}) {
-    new Zogl(async (inputText) => { return await tsjq_zhqh(inputText, outputs(), arrC, neig_kp).catch(err => { throw err }) }, equals, neig_kp).zogl()
+async function zogl_tsjq(tsjq_zhqh, outputs, arrC, equals, neig_kp = {}) {
+    return await new Zogl(async (inputText) => { return await tsjq_zhqh(inputText, outputs(), arrC, neig_kp).catch(err => { throw err }) }, equals, neig_kp)
+        .zogl().catch(err => { throw err })
 }
 module.exports = zogl_tsjq
