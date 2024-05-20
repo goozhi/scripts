190f46ca203fc3234e0d663a1282ef20056afdd8
wrvr <974054997@qq.com>
2024-05-20 17:22:12 +0800

    bvyp wydb wdbu

--- KPLU/wrvr/atvn/wrvrRfrf.js
+++ KPLU/wrvr/atvn/wrvrRfrf.js
@@ -21,7 +21,7 @@ function wrvrRfrf(neig_kp) {
     if (user_params.vv) {
         return rj_rfrf_jtyj
     } else {
-        return rj_rfrf_jtyj.replace(/(?<!,|\.|\?|['"]) (?!\w)/g, "")
+        return rj_rfrf_jtyj.replace(/(?<!,|\.|[\?\*\[\]]|['"]) (?!\w)/g, "")
         .replace(/\b(?:_|)(\w+)_\b/g,'$1')
         .replace(/\b_(\w+)(?:_|)\b/g,'$1')
 
---
+++ wydb_wdbu.js
@@ -0,0 +1,9 @@
+const Jk_ll_zv_wrm = require("./jk_ll_zv_wrm")
+
+
+const reg_dyvy_phfd = /(?:\B)```(?:(?!```)[\s\S])*\n\s{0,2}```|(?:!|)\[.*\]\(.*?\)/g
+function wydb_wdbu(rj_kp = "", wlba_jkjk_ll_wdbu) {
+    return new Jk_ll_zv_wrm(reg_dyvy_phfd, "ggg_dyvy_phfd").setBqeo(rj_kp).ymrg(wlba_jkjk_ll_wdbu)
+        .getResult()
+}
+module.exports = wydb_wdbu
