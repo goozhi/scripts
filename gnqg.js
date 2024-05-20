ed8425d4aa2525f1d41677b57a2901261a3fe7a7
wrvr <974054997@qq.com>
2024-05-20 17:21:53 +0800

    ciys gnqg

--- gnqg.js
+++ gnqg.js
@@ -3,28 +3,31 @@ const mkHtmlId = require('./mkHtmlId')
 const rfrf = require('./rfrf')
 class Gnqg {
     constructor() {
-        this.toReg = (vnwm_titles = []) => {
-            const vnwm_sort = vnwm_titles.slice(0).sort((a, b) => b.length - a.length)
-            this.reg_titles = new RegExp(vnwm_sort.map(rn1 => SturnR(rn1)).join("|"), "ig")
+        this.toReg = () => {
+            this.reg_titles = new RegExp(this.vnwm_sort_zkrs.map(rn1 => SturnR(rn1)).join("|"), "ig")
             return this.reg_titles
         }
         this.getLinks = () => this.rj_links || ""
         this.get_stgn_titles = () => []
         this.setlinks = (content = "") => {
             if (this.reg_titles.test(content)) {
-                const vnwm_stgn_titles = content.match(this.reg_titles)
-                const diwr_stgn_titles = {}
-                vnwm_stgn_titles.forEach(rn3 => {
-                    diwr_stgn_titles[rn3] = rn3
+                const vnwm_stgn_gnfo_zt = content.match(this.reg_titles)
+                const diwr_stgn_zt_ag_rr = {}
+                // const diwr_stgn_gnfo_zt = {}
+                vnwm_stgn_gnfo_zt.forEach(rn3 => {
+                    // diwr_stgn_gnfo_zt[rn3] = rn3
+                    diwr_stgn_zt_ag_rr[rn3.toLowerCase()] = true
                 })
-                this.rj_links = `\n\n${rfrf('stgn')} links\n` + Object.keys(diwr_stgn_titles).map(rn1 => `* [${rn1}](#${mkHtmlId(rn1)})`).join("\n")
-                this.get_stgn_titles = () => Object.keys(diwr_stgn_titles)
+                const vnwm_stgn_titles = this.vnwm_sort_zkrs.filter(rn1 => diwr_stgn_zt_ag_rr[rn1.toLowerCase()])
+                this.rj_links = `\n\n${rfrf('stgn')} links\n` + vnwm_stgn_titles.map(rn1 => `* [${rn1}](#${mkHtmlId(rn1)})`).join("\n")
+                this.get_stgn_titles = () => vnwm_stgn_titles
             }
             this.content = content + this.getLinks()
             return this
         }
         this.setReg = (vnwm_titles) => {
-            this.toReg(vnwm_titles)
+            this.vnwm_sort_zkrs = vnwm_titles.slice(0).sort((a, b) => b.length - a.length)
+            this.toReg()
             return this
         }
         this.addlinks = (content, vnwm_titles) => {
