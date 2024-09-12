90fdafb5f7b1b3c7016797dc5eb90d492a336d89
wrvr <974054997@qq.com>
2024-09-12 21:49:52 +0800

    add stuff

--- KPLU/wrvr/atvn/yhrj_xjvx_ld_wrvr.js
+++ KPLU/wrvr/atvn/yhrj_xjvx_ld_wrvr.js
@@ -1,4 +1,3 @@
-
 function yhrj_xjvx_ld_wrvr(yhrj = "", wrvr_kp = {}) {
     return rfrf_eysj(yhrj, wrvr_kp)
         .replace(/([\u4E00-\u9FA5])(\w)/g, '$1 $2')
@@ -11,12 +10,12 @@ function yhrj_xjvx_ld_wrvr(yhrj = "", wrvr_kp = {}) {
 module.exports = yhrj_xjvx_ld_wrvr
 
 function rfrf_eysj(yhrj_xjvx, wrvr_kp = {}) {
-    return yhrj_xjvx.split('').reduce((mb_rj, rn1, bnll_eqwy) => {
+    return yhrj_xjvx.split('').reduce((mb_rj, rn1, bnll_eqwy, arr_1) => {
         const reg_ud_pc_yhrj = /[\u4E00-\u9FA5]$/
         if (reg_ud_pc_yhrj.test(mb_rj)) {
             return mb_rj.replace(reg_ud_pc_yhrj, (_) => {
                 if (wrvr_kp.diwr_sj_di_wrvr[_ + rn1])
-                    return (bnll_eqwy ? " " : "") + wrvr_kp.diwr_sj_di_wrvr[_ + rn1]
+                    return wrvr_kp.diwr_sj_di_wrvr[_ + rn1]+(bnll_eqwy!==arr_1.length-1 ? " " : "")
                 else
                     return _ + rn1
             })
