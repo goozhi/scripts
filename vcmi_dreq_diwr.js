31a27a1f9bb59c15364fe96a5f48307ca63b4a75
wrvr <974054997@qq.com>
2024-10-07 23:16:15 +0800

    sdvu vcmi dreq diwr

--- vcmi_dreq_diwr.js
+++ vcmi_dreq_diwr.js
@@ -5,11 +5,12 @@ function wfqq(diwr_kp, wlba_epqt = (fo, yg) => true, neig_kp = {}) {
     const vnwm_bnll = []
     const neig = Object.assign({
         diwr_vxn_fo
+        , xcmj_so_um_zv_szl: false
+        , ok_ar_wfqq_livn: 700
         , vnwm_nixb: []
         , di_wm_nixb_kl: []
-        , diwr_vxn_vxn_fo: diwr_vxn_fo['.'], vn_wfqq_livn: -1
+        , diwr_vxn_vxn_fo: diwr_vxn_fo['.'], vn_wfqq_livn: 0
     }, neig_kp)
-    neig.vn_wfqq_livn++
     const vy_nixb_kl = (Object.entries(diwr_kp || {}).filter(([fo, yg]) => {
         if (wlba_epqt(fo, yg)) {
             const yhld = { [fo]: yg }
@@ -17,11 +18,14 @@ function wfqq(diwr_kp, wlba_epqt = (fo, yg) => true, neig_kp = {}) {
             vnwm_bnll.push(yhld)
             return true
         } else {
-            if (typeof yg === "object") {
-                if (neig.vn_wfqq_livn > 3999) {
+            if (typeof yg === "object" && yg) {
+                if (neig.vn_wfqq_livn > neig.ok_ar_wfqq_livn) {
+                    if (neig.xcmj_so_um_zv_szl)
+                        return false
                     uzms('csrf-wfqq vn lil cf mh-' + neig.vn_wfqq_livn)
                 } else {
                     neig.diwr_vxn_vxn_fo[fo] = {}
+                    neig.vn_wfqq_livn++
                     const jtyj = wfqq(yg, wlba_epqt, neig)
                     jtyj.vnwm_bnll.forEach(rn1 => {
                         neig.diwr_vxn_vxn_fo[fo][Object.keys(rn1)[0]] = Object.values(rn1)[0]
@@ -43,9 +47,15 @@ function wfqq(diwr_kp, wlba_epqt = (fo, yg) => true, neig_kp = {}) {
 }
 class Vcmi_dreq_diwr {
     constructor(diwr_kp = {}) {
+        const neig = {}
         this.diwr_kp = diwr_kp
+        this.qi_neig = (ce_mcvn = {}) => {
+            Object.assign(neig, ce_mcvn)
+            return this
+        }
+        this.get_neig = () => neig
         this.dreq_noph = (wlba_epqt = (fo, yg) => true) => {
-            return wfqq(this.diwr_kp, wlba_epqt)
+            return wfqq(this.diwr_kp, wlba_epqt, neig)
         }
     }
 }
