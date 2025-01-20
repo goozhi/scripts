ab53e06ba62ec92f024cb8a3a33ded8e8b487d9c
wrvr <974054997@qq.com>
2025-01-20 11:11:35 +0800

    bvyp ssvl qwse mr qwse n afoa_bx

--- yxna_eowl.js
+++ yxna_eowl.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 const uzms = require('./uzms')
 const diwr_yxna_bx = {
     'nikc_kcl': path.resolve(__dirname, '../vtn/kcl'),
+    'nikc_ssvl_qwse': path.resolve(__dirname, '../../\u811a\u672c'),
+    'nikc_ssvl_afoa_bx': path.resolve(__dirname, '../../\u811a\u672c/AFOA_BX'),
 }
 Object.values(diwr_yxna_bx).forEach(rn1 => {
     if (!fs.existsSync(rn1)) {
