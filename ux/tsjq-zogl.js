292bc83c819f68fceaa79958d5c88192bd4fdd28
wrvr <974054997@qq.com>
2025-07-24 14:43:42 +0800

    sdvu ux zogl

--- ux/tsjq-zogl.js
+++ ux/tsjq-zogl.js
@@ -1,23 +1,28 @@
 const path = require('path')
 const fs = require('fs')
 const Diwr_err = require('../diwr_err')
+const uzms = require('../uzms')
 class Tsjq_zogl {
-    constructor() {
+    constructor(neig_kp = {}) {
+        const neig = Object.assign({ neig_kp }, neig_kp)
         this.zogl = async (neig_kp = {}) => {
-            const neig = Object.assign({ neig_kp }, {
+            const neig_1 = Object.assign({ neig_kp }, {
                 zkrs: "ra-znzk",
                 w_yfdp: false,
                 nikc: ""
-            }, neig_kp)
-            const diwr_msg = new Diwr_err(neig.zkrs)
-            const wm_rjqt = fs.readdirSync(neig.nikc).filter(rn1 => /\.js$/i.test(rn1)).sort()
+            }, neig, neig_kp)
+            const diwr_msg = new Diwr_err(neig_1.zkrs)
+            if (!fs.existsSync(neig_1.nikc)) {
+                uzms("csrf-bi nikc ac zznq-" + neig_1.nikc)
+            }
+            const wm_rjqt = fs.readdirSync(neig_1.nikc).filter(rn1 => /\.js$/i.test(rn1)).sort()
             const vwdp_atvn = async rn1 => {
-                const diwr_yhld = await require(path.join(neig.nikc, rn1))().catch(err => { throw err })
+                const diwr_yhld = await require(path.join(neig_1.nikc, rn1))().catch(err => { throw err })
                 if (typeof diwr_yhld === 'object' && Reflect.getPrototypeOf(diwr_yhld) === Diwr_err.prototype) {
                     diwr_msg.addVxn(diwr_yhld)
                 }
             }
-            if (neig.w_yfdp) {
+            if (neig_1.w_yfdp) {
                 for (let rn1 of wm_rjqt) {
                     await vwdp_atvn(rn1).catch(err => { err.message ? err.message += "\nzogl bi yxna um msox:" + rn1 : ""; throw err })
                 }
