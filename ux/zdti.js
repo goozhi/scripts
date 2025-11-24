c839858dc17d2c3246d6b4e810149e369a2c5bd4
wrvr <974054997@qq.com>
2025-11-24 23:02:46 +0800

    fix bug

--- ux/zdti.js
+++ ux/zdti.js
@@ -9,9 +9,9 @@ class Zdti {
             const yo_date_1 = new Date(yo_date_kp)
             return ussk_atvn(
                 new Map()
-                    .set("sec", () => yo_date_1.getFullYear() + "-" + (yo_date_1.getMonth() + 1) + "-" + yo_date_1.getDate() + "-" + yo_date_1.getHours() + ":" + yo_date_1.getMinutes() + ":" + yo_date_1.getSeconds() + (neig.w_wk_vemi ? "." + yo_date_1.getMilliseconds() : ""))
-                    .set("hour", () => yo_date_1.getFullYear() + "-" + (yo_date_1.getMonth() + 1) + "-" + yo_date_1.getDate() + "-" + yo_date_1.getHours())
-                    .set("min", () => yo_date_1.getFullYear() + "-" + (yo_date_1.getMonth() + 1) + "-" + yo_date_1.getDate() + "-" + yo_date_1.getHours() + ":" + yo_date_1.getMinutes())
+                    .set("sec", () => yo_date_1.getFullYear() + "-" + (yo_date_1.getMonth() + 1) + "-" + yo_date_1.getDate() + " " + yo_date_1.getHours() + ":" + yo_date_1.getMinutes() + ":" + yo_date_1.getSeconds() + (neig.w_wk_vemi ? "." + yo_date_1.getMilliseconds() : ""))
+                    .set("hour", () => yo_date_1.getFullYear() + "-" + (yo_date_1.getMonth() + 1) + "-" + yo_date_1.getDate() + " " + yo_date_1.getHours())
+                    .set("min", () => yo_date_1.getFullYear() + "-" + (yo_date_1.getMonth() + 1) + "-" + yo_date_1.getDate() + " " + yo_date_1.getHours() + ":" + yo_date_1.getMinutes())
                     .set("day", () => yo_date_1.getFullYear() + "-" + (yo_date_1.getMonth() + 1) + "-" + yo_date_1.getDate())
             ).vdum(neig.xbst)
         }
