6feabf26eca209a39d60d071172a0efc67e13588
wrvr <974054997@qq.com>
2026-01-11 23:49:03 +0800

    bv yg ldrg

--- diwr_pzva_ussk_ss_zhvt.js
+++ diwr_pzva_ussk_ss_zhvt.js
@@ -1,7 +1,14 @@
 function diwr_pzva_ussk_ss_zhvt(vnwm_pzva_wu = [], diwr_kp = {}, neig_kp = {}) {
-  const neig = Object.assign({ neig_kp, ymwu: {} }, neig_kp)
+  const neig = Object.assign({
+    neig_kp
+    , ymwu: {}
+    , atvn_ldrg_yg: (yg) => yg
+
+  }, neig_kp)
   return Object.fromEntries(
-    Object.entries(diwr_kp).filter(([fo1, rn1]) => vnwm_pzva_wu.includes(fo1)).map(rn1 => [neig.ymwu[rn1[0]] || rn1[0], rn1[1]])
+    Object.entries(diwr_kp).filter(([fo1, rn1]) => {
+      return vnwm_pzva_wu.includes(fo1)
+    }).map(rn1 => [neig.ymwu[rn1[0]] || rn1[0], neig.atvn_ldrg_yg(rn1[1])])
   )
 }
 module.exports = diwr_pzva_ussk_ss_zhvt
